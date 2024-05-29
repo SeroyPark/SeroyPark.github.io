991c16871f0ff87fc7b8d39fd9abf451a3d47c37
park <jkspop12@naver.com>
2024-05-30 03:19:30 +0900

    'third'

--- js/index.js
+++ js/index.js
@@ -271,11 +271,11 @@ const subImgClick = (e, index, m = false) => {
     $('.wi' + e + ' .IMG_sub').removeClass('active');
     $('.wi' + e + ' .IMG_sub').eq(index).addClass('active');
     if (m) {
-        //$('.wi' + e + ' .IMG_main').html(`<img class='mobileImg' src='./src/img/project_img/${e}_${index}.jpg' alt='프로젝트 이미지' onclick="mainImgClick(${e}, ${index}, ${m})">`);
-        document.querySelector('.wi' + e + ' .IMG_main').outerHTML = `<div class="IMG_main" onclick="mainImgClick(${e}, ${index}, ${m})"><img class='mobileImg' src='./src/img/project_img/${e}_${index}.jpg' alt='프로젝트 이미지'></div>`;
+        //$('.wi' + e + ' .IMG_main').html(`<img class='mobileImg' src='./src/img/project_img/fitweb/${e}_${index}.png' alt='프로젝트 이미지' onclick="mainImgClick(${e}, ${index}, ${m})">`);
+        document.querySelector('.wi' + e + ' .IMG_main').outerHTML = `<div class="IMG_main" onclick="mainImgClick(${e}, ${index}, ${m})"><img class='mobileImg' src='./src/img/project_img/fitweb/${e}_${index}.png' alt='프로젝트 이미지'></div>`;
     } else {
-        //$('.wi' + e + ' .IMG_main').html(`<img src='./src/img/project_img/${e}_${index}.jpg' alt='프로젝트 이미지' onclick="mainImgClick(${e}, ${index}, ${m})">`);
-        document.querySelector('.wi' + e + ' .IMG_main').outerHTML = `<div class="IMG_main" onclick="mainImgClick(${e}, ${index}, ${m})"><img src='./src/img/project_img/${e}_${index}.jpg' alt='프로젝트 이미지'></div>`;
+        //$('.wi' + e + ' .IMG_main').html(`<img src='./src/img/project_img/fitweb/${e}_${index}.png' alt='프로젝트 이미지' onclick="mainImgClick(${e}, ${index}, ${m})">`);
+        document.querySelector('.wi' + e + ' .IMG_main').outerHTML = `<div class="IMG_main" onclick="mainImgClick(${e}, ${index}, ${m})"><img src='./src/img/project_img/fitweb/${e}_${index}.png' alt='프로젝트 이미지'></div>`;
     }
     $('.wi' + e + ' .IMG_main img').css({ opacity: '0' });
     $('.wi' + e + ' .IMG_main img').animate({ opacity: '1' }, 150);
@@ -285,9 +285,9 @@ const mainImgClick = (e, index, m = false) => {
     $('.sizeUP').addClass('visible');
     $('.sizeUP').focus();
     if (m) {
-        $('.sizeUP .ct').html(`<img class='mobileImg' src='./src/img/project_img/${e}_${index}.jpg' alt='프로젝트 이미지'>`);
+        $('.sizeUP .ct').html(`<img class='mobileImg' src='./src/img/project_img/fitweb/${e}_${index}.png' alt='프로젝트 이미지'>`);
     } else {
-        $('.sizeUP .ct').html(`<img src='./src/img/project_img/${e}_${index}.jpg' alt='프로젝트 이미지'>`);
+        $('.sizeUP .ct').html(`<img src='./src/img/project_img/fitweb/${e}_${index}.png' alt='프로젝트 이미지'>`);
     }
     $('.sizeUP .ct').scrollTop(0);
     $('html').css('overflow', 'hidden');
